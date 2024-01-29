9fc51326dfb68ebcc76337a7c7f183c1965bc8f8
Serkalem Bekele <153066138+Serki1223@users.noreply.github.com>
2024-01-29 15:10:04 -0500

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
-// import RowList from "../../components/Rows/RowList/RowList";
+// import RowList from "../../components/Rows/Row/rowList";
 
 const Home = () => {
   return (
     <>
       <Header />
-       <Banner />
-        {/* <RowList />  */}
+      <Banner />
+      {/* <RowList /> */}
       <Footer />
     </>
   );
