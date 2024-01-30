2a33ee361c42c420b28a6b2faeb5cd78b28132b8
Serkalem Bekele <153066138+Serki1223@users.noreply.github.com>
2024-01-29 22:03:22 -0500

    Row

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -23,28 +23,30 @@ const Banner = () => {
         return str?.length > n ? str.substr(0, n - 1) + '...' : str;
     }
   return (
-      <div
-          className="banner"
-          style={{
-              backgroundSize: "cover",
-              backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat"
-          }}
-      >
-          <div className="banner__contents">
-              <h1 className="banner__title">
-                  {movie?.title || movie?.name || movie?.original_name}
-              </h1>
-              <div className="banner__buttons">
-                  <button className="banner__button play">Play</button>
-                  <button className="banner__button">My List</button>
-              </div>
-              <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
-          </div>
-          <div className="banner__fadeBottom" />
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+          </h1>
+          <button className="banner__button play">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
       </div>
-  )
+      <div className="banner__fadeBottom" />
+    </div>
+  );
 }
 
 export default Banner
